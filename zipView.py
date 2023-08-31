8a8bf401acedbd58fc3550ec5630326429ac49bf
ze3ck <63827366+ze3ck@users.noreply.github.com>
2023-08-31 04:41:12 -0400

    Update zipView.py
    conexion a base de datos

--- zipView.py
+++ zipView.py
@@ -1,5 +1,6 @@
 import os
 import zipfile
+import mysql.connector
 
 BOLD = "\033[1m"
 END = "\033[0m"
@@ -19,29 +20,35 @@ banner = r"""
 print(banner)
 
 
+# Función para insertar datos en la base de datos
+def insert_data_into_database(db_connection, file_id, keyword, content):
+    with db_connection.cursor() as cursor:
+        insert_query = """
+        INSERT INTO datos_salientes (archivo_id, keyword, content)
+        VALUES (%s, %s, %s)
+        """
+        cursor.execute(insert_query, (file_id, keyword, content))
+    db_connection.commit()
+
+
+# Menu
 def menu_ordenar_archivos():
     output_file = "dumpData.txt"
-    
-    print("-"*30)
+
+    print("-" * 30)
     print("1) Extraer, formatear y almacenar archivos")
-    print("2) Mostrar archivo almacenado")
-    print("3) Salir")
-    
+
     opcion = input("> ")
 
     if opcion == "1":
         ordenar_archivos_txt()
-    elif opcion == "2":
-        mostrar_archivo(output_file)
-    elif opcion == "3":
-        salir()
     else:
         print("Opción no válida. Por favor, seleccione 1, 2 o 3.")
 
 
-def ordenar_archivos_txt():
-    # Aquí iría el código para ordenar archivos TXT
-    zip_file_path = "Search_2023-08-22 16_14_12.zip"
+# inicio de procesamiento de datos
+def ordenar_archivos_txt(db_connection):
+    zip_file_path = "Search 2023-08-22 16_14_12.zip"
     extract_dir = "Extracted_files"
 
     with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
@@ -53,56 +60,70 @@ def ordenar_archivos_txt():
                 print(name)
                 zip_ref.extract(name, path=extract_dir)
 
-    # Extracción y salida, junto con palabras clave a buscar dentro de los txt
     extracted_dir = "Extracted_files"
-
     output_file = "dumpData.txt"
 
-    keywords = ["Username", "Password", "URL"]
+    # Nuevas asignaciones de palabras clave
+    keyword_mappings = {
+        "Username": ["USER", "Login", "Username"],
+        "Password": ["Password", "PASS"],
+        "Url": ["URL", "Host"],
+    }
 
-    def find_keywords_and_content_in_file(file_path, keywords):
+    # Procesar datos
+    def find_keywords_and_content_in_file(file_path, keyword_mappings):
         found_data = []
         with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
             for line in lines:
-                found_keywords = []
-                for keyword in keywords:
-                    if keyword in line:
-                        found_keywords.append(keyword)
-                if found_keywords:
-                    found_data.append((', '.join(found_keywords), line.strip()))
+                for keyword, keywords_list in keyword_mappings.items():
+                    found_keywords = [kw for kw in keywords_list if kw in line]
+                    if found_keywords:
+                        found_data.append((keyword, line.strip()))
+                        break
         return found_data
 
+    # Ordenar la salida de los datos pertinentes
     with open(output_file, "w", encoding="utf-8") as out_f:
         txt_files = [f for f in os.listdir(extracted_dir) if f.lower().endswith(".txt")]
         txt_files.sort()
 
         for txt_file in txt_files:
             txt_file_path = os.path.join(extracted_dir, txt_file)
-            found_data = find_keywords_and_content_in_file(txt_file_path, keywords)
-
+            found_data = find_keywords_and_content_in_file(
+                txt_file_path, keyword_mappings
+            )
+            # Setear salida de datos para que sea legible
             if found_data:
-                file_id = os.path.splitext(txt_file)[
-                    0
-                ]  # Obtener la ID del archivo de origen
-                out_f.write(f"\nArchivo: {file_id}.txt\n")
+                file_id = os.path.splitext(txt_file)[0]
                 for keyword, content in found_data:
-                    out_f.write(f" {content}\n")
-                    
+                    insert_data_into_database(db_connection, file_id, keyword, content)
 
-    print("Análisis completado. Los resultados se han almacenado en 'dumpData.txt'.")
 
-def mostrar_archivo(file_path):
-    print("Mostrando archivo:")
+def main():
+    db_connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="critdb",
+    )
 
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            content = f.read()
-            print(content)
-    except FileNotFoundError:
-        print(f"El archivo '{file_path}' no fue encontrado.")
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS datos_salientes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        archivo_id VARCHAR(255),
+        keyword VARCHAR(255),
+        content TEXT
+    )
+    """
+    with db_connection.cursor() as cursor:
+        cursor.execute(create_table_query)
+    db_connection.commit()
 
-def salir():
-    print("Saliendo...")
+    ordenar_archivos_txt(db_connection)
 
-menu_ordenar_archivos()
+    db_connection.close()
+
+
+if __name__ == "__main__":
+    main()
